0bdd9b7f7ea2728a6bdf9c81f829995650b765b5
Jourdan Hamilton <jourdan.hamilton1@gmail.com>
2019-05-31 21:13:05 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -7,6 +7,7 @@ def hello_t(array)
 
   end
 
+array
 end
 
 # call your method here!
@@ -14,4 +15,4 @@ hello_t(["Tim" , "Tom" , "Jim"]) do |name|
   if name.start_with? ("T")
     puts "Hi, #{name}"
   end
-end 
+end
